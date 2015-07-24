ce732cb596d852c5474d2c9b43127e7f791c4b92
Julien Charles <julien.charles@gmail.com>
2015-07-24 14:30:22 +0200

    added most of the signatures

--- geolocate/src/main/java/fr/m2i/formation/poec/geolocate/rest/BDDService.java
+++ geolocate/src/main/java/fr/m2i/formation/poec/geolocate/rest/BDDService.java
@@ -2,21 +2,32 @@ package fr.m2i.formation.poec.geolocate.rest;
 
 import java.util.List;
 
+import fr.m2i.formation.poec.geolocate.domain.Address;
 import fr.m2i.formation.poec.geolocate.domain.LocatedObject;
+import fr.m2i.formation.poec.geolocate.domain.Tag;
 
 
 public interface BDDService {
 
-	Integer getLocationCount();
 
-	List<LocatedObject> getLocations(int start);
+        Integer getLocationCount();
 
-	LocatedObject[] getLocation(Double latitude, Double longitude);
+        List<LocatedObject> getLocatedObjects(int start, int step);
+        List<LocatedObject> getLocatedObjects(Tag tag, int start, int step);
 
-	LocatedObject[] getLocation(Double latitude, Double longitude,
-			Double altitude);
 
-	LocatedObject[] getLocation(String uuid);
-	
+        LocatedObject[] getLocatedObjects(Double latitude, Double longitude);
+
+        LocatedObject[] getLocatedObjects(Double latitude, Double longitude,
+                        Double altitude);
+
+        LocatedObject getLocatedObject(String uuid);
+        Tag getTag(String name);
+        Address[] getAddresses(int start, int step);
+        Address getAddress(String uuid);
+
+        void insert(LocatedObject lo);
+
 
 }
+
