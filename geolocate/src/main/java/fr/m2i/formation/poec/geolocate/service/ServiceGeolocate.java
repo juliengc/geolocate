512d57c457410e33c9e47e95f262dcfee7499338
sandraroche <sandra.roche.nice@gmail.com>
2015-07-27 10:51:31 +0200

    Added implementation of BDDService

---
+++ geolocate/src/main/java/fr/m2i/formation/poec/geolocate/service/ServiceGeolocate.java
@@ -0,0 +1,104 @@
+package fr.m2i.formation.poec.geolocate.service;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+
+import fr.m2i.formation.poec.geolocate.domain.Address;
+import fr.m2i.formation.poec.geolocate.domain.LocatedObject;
+import fr.m2i.formation.poec.geolocate.domain.Tag;
+
+@Stateless
+public class ServiceGeolocate implements BDDService {
+
+	@Override
+	public Integer getLocatedObjectsCount() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<LocatedObject> getLocatedObjects(int start, int step) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getLocatedObjectsCount(Tag tag) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<LocatedObject> getLocatedObjects(Tag tag, int start, int step) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<LocatedObject> getLocatedObjects(double latitude,
+			double longitude) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<LocatedObject> getLocatedObjects(double latitude,
+			double longitude, double altitude) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LocatedObject getLocatedObject(String uuid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Tag getTag(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Address> getAddresses(int start, int step) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Address getAddress(String uuid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(LocatedObject lo) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Tag> getTags(int start, int step) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getLocatedObjectsInAreaCount(double latitude1,
+			double longitude1, double latitude2, double longitude2,
+			List<Tag> tags) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<LocatedObject> getLocatedObjectsInArea(double latitude1,
+			double longitude1, double latitude2, double longitude2,
+			List<Tag> tags, int start, int step) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
