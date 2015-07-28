61b4e7e9ff216f0c6aa3ae10818d99d706c7cafc
catherine <catherine.gourdinsg@gmail.com>
2015-07-28 14:59:21 +0200

    Fixed the persist?

--- geolocate/src/main/java/fr/m2i/formation/poec/geolocate/domain/Tag.java
+++ geolocate/src/main/java/fr/m2i/formation/poec/geolocate/domain/Tag.java
@@ -1,9 +1,12 @@
 package fr.m2i.formation.poec.geolocate.domain;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Tag {
@@ -13,6 +16,8 @@ public class Tag {
 	private Long id;
 	private String name;
 	
+	@ManyToMany(mappedBy="tags")
+	Set<LocatedObject> objects;
 	
 	public Tag() {
 		
