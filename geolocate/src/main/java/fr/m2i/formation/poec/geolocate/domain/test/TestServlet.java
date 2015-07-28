86cd211df4571fc58458d1dbbc46de884563ff7f
catherine <catherine.gourdinsg@gmail.com>
2015-07-28 16:45:39 +0200

    Added more tests.

--- geolocate/src/main/java/fr/m2i/formation/poec/geolocate/domain/test/TestServlet.java
+++ geolocate/src/main/java/fr/m2i/formation/poec/geolocate/domain/test/TestServlet.java
@@ -38,6 +38,7 @@ public class TestServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println((bdd.getTags()));
+		bdd.creationTagTest();
 	}
 
 	/**
