93a4b7ab2a24f1eef2887f133dcc502a34a20d78
catherine <catherine.gourdinsg@gmail.com>
2015-07-28 13:46:59 +0200

    Made things work.

--- geolocate/src/main/java/fr/m2i/formation/poec/geolocate/service/ServiceGeolocate2.java
+++ geolocate/src/main/java/fr/m2i/formation/poec/geolocate/service/ServiceGeolocate2.java
@@ -2,9 +2,12 @@ package fr.m2i.formation.poec.geolocate.service;
 
 import java.util.List;
 
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.NonUniqueResultException;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
@@ -12,9 +15,12 @@ import fr.m2i.formation.poec.geolocate.domain.Address;
 import fr.m2i.formation.poec.geolocate.domain.LocatedObject;
 import fr.m2i.formation.poec.geolocate.domain.Tag;
 
+
+@Stateless
+@LocalBean
 public class ServiceGeolocate2  implements BDDService  {
 	
-	//@PersistenceContext(unitName="geolocatePU")
+	@PersistenceContext(unitName="geolocatePU")
 	private EntityManager em;
 
 	@Override
