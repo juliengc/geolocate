6e337875579916745eb25a957894e5e776518bd8
Edoardo Gaballo <edgar86cru@gmail.com>
2015-07-29 11:08:12 +0200

    Merged

--- geolocate/src/main/java/fr/m2i/formation/poec/geolocate/service/ServiceGeolocate.java
+++ geolocate/src/main/java/fr/m2i/formation/poec/geolocate/service/ServiceGeolocate.java
@@ -1,5 +1,6 @@
 package fr.m2i.formation.poec.geolocate.service;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -15,185 +16,42 @@ import fr.m2i.formation.poec.geolocate.domain.Address;
 import fr.m2i.formation.poec.geolocate.domain.LocatedObject;
 import fr.m2i.formation.poec.geolocate.domain.Tag;
 
-@Stateless
-@LocalBean
-public class ServiceGeolocate implements BDDService {
 
-	@PersistenceContext(unitName="geolocatePU")
+ abstract class ServiceGeolocate implements BDDService {
+
+	//@PersistenceContext(unitName="geolocatePU")
 	private EntityManager em;
 
-	@Override
-	public Integer getLocatedObjectsCount() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<LocatedObject> getLocatedObjects(int start, int step) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Integer getLocatedObjectsCount(Tag tag) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<LocatedObject> getLocatedObjects(Tag tag, int start, int step) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<LocatedObject> getLocatedObjects(double latitude,
-			double longitude) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<LocatedObject> getLocatedObjects(double latitude,
-			double longitude, double altitude) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public LocatedObject getLocatedObject(String uuid) {
-
-		LocatedObject oneLocated = null;
-
-		try{
-			oneLocated =  em.createQuery("SELECT lo from LocatedObject lo WHERE lo.uuid=:uuid ",LocatedObject.class)
-					.setParameter("uuid", uuid)
-					.getSingleResult();
-		}catch ( NoResultException | NonUniqueResultException ex){
-			throw new BDDException("Object Located Not Found");
-		}
-
-		return oneLocated;
-	}
-
-
-	@Override
-	public List<Address> getAddresses(int start, int step) {
-		// TODO Auto-generated method stub
-		/*return em.createQuery("SELECT a from Adress LIMIT :step OFFSET :start ",Address.class)
-				.setParameter("start", start*step)
-				.setParameter("step",step)
-				.getResultList();*/
-
-		return em.createQuery("SELECT a from Adress a",Address.class)
-				.setFirstResult(start*step)
-				.setMaxResults(step)
-				.getResultList();
-	}
-
-	@Override
-	public Address getAddress(String uuid) {
-		// TODO Auto-generated method stub
-		return em.createQuery("SELECT a from Address a WHERE a.uuid = :uid ",Address.class)
-				.setParameter("uid",uuid)
-				.getSingleResult();
-	}
-
-	public Address getAddress(String street, String zipCode, String city, String country) {
-		// TODO Auto-generated method stub
-		return em.createQuery("SELECT a from Address a WHERE "
-				+ "a.street = :street "
-				+ "a.zipcode = :zipcode "
-				+ "a.city = :city "
-				+ "a.country = :country ",Address.class)
-				.setParameter("street",street)
-				.setParameter("zipcode",zipCode)
-				.setParameter("city", city)
-				.setParameter("country",country)
-				.getSingleResult();
-	}
-
-	@Override
-	public void insert(LocatedObject lo) {
-		// TODO Auto-generated method stub
-		Set<Tag> loTags = new HashSet<>();
-
-		if(lo.getTags() != null) {
-			loTags = lo.getTags();
-			lo.setTags(new HashSet<>());
-		}
-
-		if(lo.getAddresses()!=null)
-		{
-			try{
-
-				Address addrLo = getAddress(lo.getAddresses().getStreet(),lo.getAddresses().getZipcode(),
-						lo.getAddresses().getCity(), lo.getAddresses().getCountry());
-
-				lo.setAddresses(addrLo);
-
-			} catch(NoResultException e) {
-
-				/*em.persist(lo.getAddresses());
-
-				Address addrLo2 = getAddress(lo.getAddresses().getStreet(),lo.getAddresses().getZipcode(),
-						lo.getAddresses().getCity(), lo.getAddresses().getCountry());
-
-				lo.setAddresses(addrLo2);*/
-			}
-		}
-
-		em.persist(lo);
-		
-		lo.setTags(loTags);
-		
-		em.merge(lo);
-	}
-
-	@Override
-	public Tag getTag(String name) {
-		// TODO Auto-generated method stub
-		return em.createQuery("SELECT t from Tag t WHERE t.name = :name ",Tag.class)
-				.setParameter("name", name)
-				.getSingleResult();
-	}
-
-	@Override
-	public List<Tag> getTags(int start, int step) {
-		// TODO Auto-generated method stub
-		/*return em.createQuery("SELECT t from Tag LIMIT :step OFFSET :start ",Tag.class)
-				.setParameter("start", start*step)
-				.setParameter("step",step)
-				.getResultList();*/
-
-		return em.createQuery("SELECT t from Tag t",Tag.class)
-				.setFirstResult(start*step)
-				.setMaxResults(step)
-				.getResultList();
-
-	}
-
-	public List<Tag> getTags() {
-		// TODO Auto-generated method stub
-		return em.createQuery("SELECT t from Tag",Tag.class).getResultList();
-	}
-
+	
 
 	@Override
 	public int getLocatedObjectsInAreaCount(double latitude1,
 			double longitude1, double latitude2, double longitude2,
 			List<Tag> tags) {
-		Integer countValues =	(Integer) em.createNativeQuery("SELECT COUNT(*) FROM LocatedObject lo WHERE"
-				+ "(lo.latitude BETWEEN :latitude1 AND :latitude2 ) "
-				+ "AND (lo.longitude BETWEEN :longitude1 AND :longitude2 ) "
-				+ "lo.tags IN :tags ")
-				.setParameter("latitude1", latitude1)
-				.setParameter("longitude1", longitude1)
-				.setParameter("latitude2", latitude2)
-				.setParameter("longitude2", longitude2)
-				.setParameter("tags", tags)
-				.getSingleResult();
-		return  countValues;
+		try{
+			Integer countValues =	(Integer) em.createNativeQuery("SELECT COUNT(*) FROM LocatedObject lo WHERE"
+					+ "(lo.latitude BETWEEN :latitude1 AND :latitude2 ) "
+					+ "AND (lo.longitude BETWEEN :longitude1 AND :longitude2 ) "
+					+ "lo.tags IN :tags ")
+					.setParameter("latitude1", latitude1)
+					.setParameter("longitude1", longitude1)
+					.setParameter("latitude2", latitude2)
+					.setParameter("longitude2", longitude2)
+					.setParameter("tags", tags)
+					.getSingleResult();
+			return  countValues;
+
+		} 
+		catch (NullPointerException e1) {
+			throw e1;
+		}
+		catch (IllegalArgumentException e2) {
+			throw e2;
+		}
+		catch(Exception ex){
+			throw new BDDException(ex.getMessage());
+		}
+
 	}
 
 	@Override
@@ -201,85 +59,95 @@ public class ServiceGeolocate implements BDDService {
 			double longitude1, double latitude2, double longitude2,
 			List<Tag> tags, int start, int step) {
 		// TODO Auto-generated method stub
-		return em.createQuery("SELECT lo FROM LocatedObject lo WHERE"
-				+ "(lo.latitude BETWEEN :latitude1 AND :latitude2 ) "
-				+ "AND (lo.longitude BETWEEN :longitude1 AND :longitude2 )"
-				+ "lo.tags IN :tags ", LocatedObject.class)
-				.setParameter("latitude1", latitude1)
-				.setParameter("longitude1", longitude1)
-				.setParameter("latitude2", latitude2)
-				.setParameter("longitude2", longitude2)
-				.setParameter("tags", tags)
-				.setFirstResult(start*step)
-				.setMaxResults(step)
-				.getResultList();
+		try {
+			List<LocatedObject> list =  em.createQuery("SELECT lo FROM LocatedObject lo WHERE"
+					+ "(lo.latitude BETWEEN :latitude1 AND :latitude2 ) "
+					+ "AND (lo.longitude BETWEEN :longitude1 AND :longitude2 )"
+					+ "lo.tags IN :tags ", LocatedObject.class)
+					.setParameter("latitude1", latitude1)
+					.setParameter("longitude1", longitude1)
+					.setParameter("latitude2", latitude2)
+					.setParameter("longitude2", longitude2)
+					.setParameter("tags", tags)
+					.setFirstResult(start*step)
+					.setMaxResults(step)
+					.getResultList();
+
+			return list;
+
+		} 
+		catch (NullPointerException e1) {
+			throw e1;
+		}
+		catch (IllegalArgumentException e2) {
+			throw e2;
+		}
+		catch(Exception ex){
+			throw new BDDException(ex.getMessage());
+		}
+
 	}
 
-	@Override
 	public int getLocatedObjectsInAreaCountStr(double latitude1,
 			double longitude1, double latitude2, double longitude2,
 			List<String> tags) {
-		Integer countValues =	(Integer) em.createNativeQuery("SELECT COUNT(*) FROM LocatedObject lo WHERE"
-				+ "(lo.latitude BETWEEN :latitude1 AND :latitude2 ) "
-				+ "AND (lo.longitude BETWEEN :longitude1 AND :longitude2 ) "
-				+ "lo.tags.name IN :tags ")
-				.setParameter("latitude1", latitude1)
-				.setParameter("longitude1", longitude1)
-				.setParameter("latitude2", latitude2)
-				.setParameter("longitude2", longitude2)
-				.setParameter("tags", tags)
-				.getSingleResult();
-		return  countValues;
+		try{
+			Integer countValues =	(Integer) em.createNativeQuery("SELECT COUNT(*) FROM LocatedObject lo WHERE"
+					+ "(lo.latitude BETWEEN :latitude1 AND :latitude2 ) "
+					+ "AND (lo.longitude BETWEEN :longitude1 AND :longitude2 ) "
+					+ "lo.tags.name IN :tags ")
+					.setParameter("latitude1", latitude1)
+					.setParameter("longitude1", longitude1)
+					.setParameter("latitude2", latitude2)
+					.setParameter("longitude2", longitude2)
+					.setParameter("tags", tags)
+					.getSingleResult();
+			return  countValues;
+		} 
+		catch (NullPointerException e1) {
+			throw e1;
+		}
+		catch (IllegalArgumentException e2) {
+			throw e2;
+		}
+		catch(Exception ex){
+			throw new BDDException(ex.getMessage());
+		}
 	}
 
-	@Override
 	public List<LocatedObject> getLocatedObjectsInAreaStr(double latitude1,
 			double longitude1, double latitude2, double longitude2,
 			List<String> tags, int start, int step) {
 		// TODO Auto-generated method stub
-		return em.createQuery("SELECT lo FROM LocatedObject lo WHERE"
-				+ "(lo.latitude BETWEEN :latitude1 AND :latitude2 ) "
-				+ "AND (lo.longitude BETWEEN :longitude1 AND :longitude2 )"
-				+ "lo.tags.name IN :tags ", LocatedObject.class)
-				.setParameter("latitude1", latitude1)
-				.setParameter("longitude1", longitude1)
-				.setParameter("latitude2", latitude2)
-				.setParameter("longitude2", longitude2)
-				.setParameter("tags", tags)
-				.setFirstResult(start*step)
-				.setMaxResults(step)
-				.getResultList();
+		try{
+
+			List<LocatedObject> list = em.createQuery("SELECT lo FROM LocatedObject lo WHERE"
+					+ "(lo.latitude BETWEEN :latitude1 AND :latitude2 ) "
+					+ "AND (lo.longitude BETWEEN :longitude1 AND :longitude2 )"
+					+ "lo.tags.name IN :tags ", LocatedObject.class)
+					.setParameter("latitude1", latitude1)
+					.setParameter("longitude1", longitude1)
+					.setParameter("latitude2", latitude2)
+					.setParameter("longitude2", longitude2)
+					.setParameter("tags", tags)
+					.setFirstResult(start*step)
+					.setMaxResults(step)
+					.getResultList();
+
+			return list;
+
+		} 
+		catch (NullPointerException e1) {
+			throw e1;
+		}
+		catch (IllegalArgumentException e2) {
+			throw e2;
+		}
+		catch(Exception ex){
+			throw new BDDException(ex.getMessage());
+		}
 	}
 
-	@Override
-	public List<LocatedObject> getLocatedObjects(String substring, int start,
-			int step) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Tag> getTagsLike(String substring, int start, int step) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Integer getAddressesCount() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void insertAddress(Address ad) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Integer getTagsCount() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 
 }
