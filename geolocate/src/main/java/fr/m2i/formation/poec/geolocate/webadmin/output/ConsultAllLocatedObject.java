706aa8bc50af89cba9dc11d8dba79701f7dc1bc1
sandraroche <sandra.roche.nice@gmail.com>
2015-07-29 14:24:31 +0200

    update output class and interface

--- geolocate/src/main/java/fr/m2i/formation/poec/geolocate/webadmin/output/ConsultAllLocatedObject.java
+++ geolocate/src/main/java/fr/m2i/formation/poec/geolocate/webadmin/output/ConsultAllLocatedObject.java
@@ -1,27 +1,22 @@
 package fr.m2i.formation.poec.geolocate.webadmin.output;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.io.Serializable;
 import java.util.List;
-import java.util.Set;
 import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import fr.m2i.formation.poec.geolocate.domain.Address;
 import fr.m2i.formation.poec.geolocate.domain.LocatedObject;
-import fr.m2i.formation.poec.geolocate.domain.Tag;
 import fr.m2i.formation.poec.geolocate.service.BDDServiceImpl;
 
 
 @Named("consultAll")
-@RequestScoped
-public class ConsultAllLocatedObject {
+@ViewScoped
+public class ConsultAllLocatedObject implements Serializable{
 
 	private static final Logger logger = Logger.getLogger(ConsultAllLocatedObject.class.getName());  
 	
@@ -33,9 +28,45 @@ public class ConsultAllLocatedObject {
 	
 	private List<LocatedObject> allObjects;
 	private String oneTag;
-	private Integer step = 50;
+	private Integer step = 5;
+	private Integer countPage = 1;
+	private Integer indexPage = 1;
+	
+	public Integer getStartElem(){
+		return ((indexPage-1)*step);
+	}
+	
+	public boolean getShowBack(){
+		return (countPage >  1 && indexPage > 1); 
+	}
+	
+	public boolean getShowNext(){
+		return (countPage >  1 && (indexPage < countPage)  ); 
+	}
 	
 	
+	public Integer getIndexPage() {
+		logger.info("Get Index Page "+indexPage+" - " + FacesContext.getCurrentInstance().getCurrentPhaseId() );
+		return indexPage;
+	}
+
+	public void setIndexPage(Integer indexPage) {
+		this.indexPage = indexPage;
+	}
+
+	public Integer getCountPage() {
+		logger.info("Get Count Page "+countPage);
+		
+		return countPage;
+	}
+
+	public void setCountPage(Integer countPage) {
+		if ( countPage<=0)
+			this.countPage = 1;
+		else
+			this.countPage = countPage;
+	}
+
 	public String getOneTag() {
 		logger.info("Get One tag <--- "+oneTag);
 		return oneTag;
@@ -47,12 +78,12 @@ public class ConsultAllLocatedObject {
 	}
 
 	public List<LocatedObject> getAllObjects() {
-		logger.info("get getAllObjects ---> ");
+		logger.info("get getAllObjects ---> "+allObjects.size());
 		return allObjects;
 	}
 
 	public void setAllObjects(List<LocatedObject> allObjects) {
-		logger.info("set setAllObjects ---> ");
+		logger.info("set setAllObjects ---> "+allObjects.size());
 		this.allObjects = allObjects;
 	}
 
@@ -60,45 +91,54 @@ public class ConsultAllLocatedObject {
 	void init(){
 		
 		logger.info("ConsultAllLocatedObject --> init");
-		/*allObjects = new ArrayList<>();
 		
-		allObjects.add("object1");
-		allObjects.add("object2");
-		allObjects.add("object3");
-		allObjects.add("object4");
-		allObjects.add("object5");
-		allObjects.add("object6");
-		allObjects.add("object7");
-		allObjects.add("object8");
-		allObjects.add("object9");
-		allObjects.add("object10");
-		*/
-		
-	/*	LocatedObject objectLocated = new LocatedObject("object1 name", "description object 1", 2.0, 222.0, new Address("streee \n eeeee", "06200", "City", "state", "country"));
-		logger.info("ConsultAllLocatedObject --> init NEW");
-		objectLocated.setAltitude(0);
-		logger.info("ConsultAllLocatedObject --> init setAltitude");
-		Set<Tag> tags = new HashSet<>();
-		
-		tags.add(new Tag("tag11"));
-		tags.add(new Tag("tag22"));
-		
-		logger.info("ConsultAllLocatedObject --> init new TAG");
-		objectLocated.setTags(tags);
-		logger.info("ConsultAllLocatedObject --> init Add TAG");
-		allObjects.add(objectLocated);
-		*/
-		allObjects =  locatedObjectService.getLocatedObjects(0, 0);
+		setAllObjects(locatedObjectService.getLocatedObjects(getStartElem(), step));
+		setCountPage((int)Math.round( (double)locatedObjectService.getLocatedObjectsCount()/(double)step));
 	
-		logger.info("ConsultAllLocatedObject --> init END");
+		logger.info("ConsultAllLocatedObject --> init END nbPage :" + countPage.toString()+", nbObject : "+allObjects.size());
 	}
 	
 	
 	public void doFilter(){
 		logger.info("ConsultAllLocatedObject --> doFilter " +oneTag);
-		setAllObjects(locatedObjectService.getLocatedObjects(oneTag, 0, 0) );
 		
-		logger.info("ConsultAllLocatedObject --> doFilter " +allObjects.size());
+		if ( oneTag!= null && !oneTag.isEmpty()){
+			setAllObjects(locatedObjectService.getLocatedObjects(oneTag, getStartElem(), step) );
+			setCountPage( (int)Math.round( (double)locatedObjectService.getLocatedObjectsLikeCount(oneTag)/(double)step));
+		}
+		else{
+			setAllObjects(locatedObjectService.getLocatedObjects(getStartElem(), step) );
+			setCountPage( (int)Math.round( (double)locatedObjectService.getLocatedObjectsCount()/(double)step));
+		}
+		
+		indexPage=1;
+		logger.info("ConsultAllLocatedObject --> doFilter nbObject: " +allObjects.size()+" nbPage "+countPage );
+	}
+	
+	public void doBack(){
+		logger.info("ConsultAllLocatedObject doBack --> " +indexPage);
+		indexPage--;
+		if ( oneTag!= null && !oneTag.isEmpty() ){
+			setAllObjects(locatedObjectService.getLocatedObjects(oneTag, getStartElem(), step) );
+		}
+		else{
+			setAllObjects(locatedObjectService.getLocatedObjects(getStartElem(), step) );
+		}
+		logger.info("ConsultAllLocatedObject doBack END--> " +indexPage);
+	}
+	
+	public void doNext(){
+		logger.info("ConsultAllLocatedObject doNext --> " +indexPage);
+		indexPage++;
+		
+		if ( oneTag != null && !oneTag.isEmpty() ){
+			setAllObjects(locatedObjectService.getLocatedObjects(oneTag, getStartElem(), step) );
+		}
+		else{
+			setAllObjects(locatedObjectService.getLocatedObjects(getStartElem(), step) );
+		}
+		
+		logger.info("ConsultAllLocatedObject doNext END--> " +indexPage);
 	}
 	
 }
