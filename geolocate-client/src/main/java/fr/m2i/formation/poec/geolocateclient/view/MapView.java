6eb5cd5a3d8c37e74af7100d44f6c1b11b1c958c
Edoardo Gaballo <edgar86cru@gmail.com>
2015-07-31 15:32:49 +0200

    Merged

--- geolocate-client/src/main/java/fr/m2i/formation/poec/geolocateclient/view/MapView.java
+++ geolocate-client/src/main/java/fr/m2i/formation/poec/geolocateclient/view/MapView.java
@@ -1,6 +1,7 @@
 package fr.m2i.formation.poec.geolocateclient.view;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -9,7 +10,10 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
 
+import org.primefaces.event.SelectEvent;
 import org.primefaces.event.map.GeocodeEvent;
 import org.primefaces.event.map.StateChangeEvent;
 import org.primefaces.model.map.DefaultMapModel;
@@ -18,6 +22,10 @@ import org.primefaces.model.map.LatLng;
 import org.primefaces.model.map.LatLngBounds;
 import org.primefaces.model.map.MapModel;
 import org.primefaces.model.map.Marker;
+import org.primefaces.model.tagcloud.DefaultTagCloudItem;
+import org.primefaces.model.tagcloud.DefaultTagCloudModel;
+import org.primefaces.model.tagcloud.TagCloudItem;
+import org.primefaces.model.tagcloud.TagCloudModel;
 
 import fr.m2i.formation.poec.geolocateclient.domain.LocatedObject;
 import fr.m2i.formation.poec.geolocateclient.rest.RestClient;
@@ -35,17 +43,22 @@ public class MapView  implements Serializable  {
 	private MapModel modelMap;
 	private LatLngBounds currentArea;
 	private String centerGeoMap;
+	private TagCloudModel modelTagCloud;
 
 	private String title;
 
 	private String inputOneTag;
 	private String inputTags;
 	
+	@DecimalMin("-90.00") @DecimalMax("90.00")
 	private double lat;
+	
+	@DecimalMin("-180.00") @DecimalMax("180.00")
 	private double lng;
 	
 	private String address;
 	
+	private List<String> tags;
 	
 	public String getAddress() {
 		return address;
@@ -56,8 +69,12 @@ public class MapView  implements Serializable  {
 		this.address = address;
 	}
 
+	@DecimalMin("-90.00") @DecimalMax("90.00")
 	private double lati;
+	
+	@DecimalMin("-180.00") @DecimalMax("180.00")
 	private double lngi;
+	
 	private int zoom;
 	private List<LocatedObject> allObjects;
 
@@ -70,9 +87,25 @@ public class MapView  implements Serializable  {
 		lngi = 7.0174095;
 		zoom = 13;
 		centerGeoMap = Double.toString(lati)+ "," +Double.toString(lngi);
+		
+		modelTagCloud = new DefaultTagCloudModel();
+		
+		modelTagCloud.addTag(new DefaultTagCloudItem("Transformers", 1));
+		modelTagCloud.addTag(new DefaultTagCloudItem("RIA", "#", 3));
+        modelTagCloud.addTag(new DefaultTagCloudItem("AJAX", 2));
+        modelTagCloud.addTag(new DefaultTagCloudItem("jQuery", "#", 5));
+        modelTagCloud.addTag(new DefaultTagCloudItem("NextGen", 4));
+        modelTagCloud.addTag(new DefaultTagCloudItem("JSF 2.0", "#", 2));
+        modelTagCloud.addTag(new DefaultTagCloudItem("FCB", 5));
+        modelTagCloud.addTag(new DefaultTagCloudItem("Mobile",  3));
+        modelTagCloud.addTag(new DefaultTagCloudItem("Themes", "#", 4));
+        modelTagCloud.addTag(new DefaultTagCloudItem("Rocks", "#", 1));
+		
+		inputTags = "";
+        tags = new ArrayList<String>();
 
-		/*LoadedAllObjects();
-		generateMarkers();*/
+		LoadedAllObjects();
+		generateMarkers();
 	}
 
 
@@ -147,6 +180,32 @@ public class MapView  implements Serializable  {
 	public void setCenterGeoMap(String centerGeoMap) {
 		this.centerGeoMap = centerGeoMap;
 	}
+	
+    public TagCloudModel getModelTagCloud() {
+        return modelTagCloud;
+    }
+     
+    public void onSelect(SelectEvent event) {
+        TagCloudItem item = (TagCloudItem) event.getObject();
+        
+        inputTags = "";
+        if (! tags.contains(item.getLabel())) {
+        	tags.add(item.getLabel());
+        }
+        for (String string : tags) {
+			if (inputTags.isEmpty()) {
+				inputTags = string;
+			} else {
+				inputTags = inputTags + "," + string;
+			}
+				
+		}
+    }
+    
+    public void clearInputTags() {
+		inputTags = "";
+        tags = new ArrayList<String>();
+    }
 
 
 	//Don't use now : function of test
@@ -208,6 +267,10 @@ public class MapView  implements Serializable  {
 	public void generateMarkers(){
 		logger.info("Generate Markers");
 
+		if(allObjects == null) {
+			return;
+		}
+		
 		getModelMap().getMarkers().clear(); //getModelMap().clearMarkers()
 
 		for (LocatedObject locatedObject : allObjects) {
@@ -215,7 +278,7 @@ public class MapView  implements Serializable  {
 			, locatedObject.getName()
 			//));
 			, locatedObject.getDescription()
-			, "http://www.google.com/mapfiles/kml/paddle/"+locatedObject.getName().toUpperCase().charAt(0)+".png"));/**/
+			, "http://www.google.com/mapfiles/kml/paddle/"+locatedObject.getName().trim().toUpperCase().charAt(0)+".png"));/**/
 		}
 		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Marker Added", "Lat:" + lati + ", Lng:" + lngi));
 	}
